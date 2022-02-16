dbe531bea438419340110962740c08c6bc4868cc
jeremy2212landa <jeremy2212landa@gmail.com>
2022-02-15 22:59:41 -0400

    Revert "migrations" This reverts commit 364e6772a0e4c208e2a1ed61492bddb0737e08b3.

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,3 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
-
-//Route::resource('')->middleware('auth:sanctum', 'verified')->name('cve');
